02f3072d97c963d3007f637f2f3b00d16978a8b1
kirjava113 <annaluizalyjak@gmail.com>
2017-11-19 10:26:27 +0100

    listeners and services

---
+++ AndroidStudioProjects/Inzynierka/app/src/main/java/lyjak/anna/inzynierka/service/broadcasts/LocationReceiver.java
@@ -0,0 +1,64 @@
+package lyjak.anna.inzynierka.service.broadcasts;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.location.Location;
+import android.location.LocationManager;
+import android.util.Log;
+import android.widget.Toast;
+
+import java.util.Date;
+
+import io.realm.RealmList;
+import lyjak.anna.inzynierka.service.model.realm.RealmLocation;
+import lyjak.anna.inzynierka.service.model.realm.Route;
+
+/**
+ * Created by Anna Łyjak on 22.09.2017.
+ */
+
+public class LocationReceiver extends BroadcastReceiver {
+
+    private static final String TAG = LocationReceiver.class.getSimpleName();
+
+    public static final Route actuallRoute = new Route();
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Log.i(TAG, "Start onRecive");
+
+        if(actuallRoute.getStartDate() == null) {
+            actuallRoute.setStartDate(new Date(System.currentTimeMillis()));
+            actuallRoute.setLocations(new RealmList<RealmLocation>());
+        }
+
+        String locationKey = LocationManager.KEY_LOCATION_CHANGED;
+        String providerKey = LocationManager.KEY_PROVIDER_ENABLED;
+        if (intent.hasExtra(providerKey)) {
+            if (!intent.getBooleanExtra(providerKey, true)) {
+                Toast.makeText(context, "disabled", Toast.LENGTH_SHORT).show();
+                Log.i(TAG, "disabled");
+            } else {
+                Toast.makeText(context, "enabled", Toast.LENGTH_SHORT).show();
+                Log.i(TAG, "enabled");
+            }
+        }
+
+        if (intent.hasExtra(locationKey)) {
+            Location loc = (Location) intent.getExtras().get(locationKey);
+            Toast.makeText(context, "Location changed: Lat: " + loc.getLatitude() +
+                    " Lng: " + loc.getLongitude(), Toast.LENGTH_SHORT).show();
+            store(loc);
+        }
+    }
+
+    private void store(Location loc) {
+        Log.i(TAG, "start store");
+        RealmLocation tempRelLoc = new RealmLocation();
+        tempRelLoc.setLatitude(loc.getLatitude());
+        tempRelLoc.setLongitude(loc.getLongitude());
+        actuallRoute.addLocationToList(tempRelLoc);
+        Log.i(TAG, "end store");
+    }
+}
---
+++ AndroidStudioProjects/Inzynierka/app/src/main/java/lyjak/anna/inzynierka/viewmodel/listeners/OnLocationServiceListener.java
@@ -0,0 +1,20 @@
+package lyjak.anna.inzynierka.viewmodel.listeners;
+
+import android.view.View;
+
+import lyjak.anna.inzynierka.view.fragments.LocationListenerFragment;
+
+/**
+ * Created by Anna Łyjak on 01.10.2017.
+ */
+
+/**
+ * This interface must be implemented by activities that contain {@link LocationListenerFragment}
+ * fragment to allow an interaction in this fragment to be communicated
+ * to the activity and potentially other fragments contained in that
+ * activity.
+ */
+public interface OnLocationServiceListener {
+    void onStartLocationService(View view);
+    void onStopLocationService(View view);
+}
